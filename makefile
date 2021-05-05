79ec27f1b2bb7115074286947ef9b65561a980fb
heeki <heekipark@gmail.com>
2021-05-05 13:19:26 -0400

    feat(example): added redis to fn

--- makefile
+++ makefile
@@ -1,16 +1,16 @@
 include etc/environment.sh
 
-sam: sam.package sam.deploy
-sam.build:
-	sam build --profile ${PROFILE} --template ${SAM_TEMPLATE} --parameter-overrides ${SAM_PARAMS} --build-dir build --manifest requirements.txt --use-container
-	sam package -t build/template.yaml --output-template-file ${SAM_OUTPUT} --s3-bucket ${S3BUCKET}
-sam.package:
-	sam package -t ${SAM_TEMPLATE} --output-template-file ${SAM_OUTPUT} --s3-bucket ${S3BUCKET}
-sam.deploy:
-	sam deploy -t ${SAM_OUTPUT} --stack-name ${SAM_STACK} --parameter-overrides ${SAM_PARAMS} --capabilities CAPABILITY_NAMED_IAM
-sam.local.invoke:
-	sam local invoke --profile ${PROFILE} -t ${SAM_TEMPLATE} --parameter-overrides ${SAM_PARAMS} --env-vars etc/environment.json -e etc/local.json FnProducer | jq
-sam.invoke:
+obs: obs.package obs.deploy
+obs.build:
+	sam build --profile ${PROFILE} --template ${OBS_TEMPLATE} --parameter-overrides ${OBS_PARAMS} --build-dir build --manifest requirements.txt --use-container
+	sam package -t build/template.yaml --output-template-file ${OBS_OUTPUT} --s3-bucket ${S3BUCKET}
+obs.package:
+	sam package -t ${OBS_TEMPLATE} --output-template-file ${OBS_OUTPUT} --s3-bucket ${S3BUCKET}
+obs.deploy:
+	sam deploy -t ${OBS_OUTPUT} --stack-name ${OBS_STACK} --parameter-overrides ${OBS_PARAMS} --capabilities CAPABILITY_NAMED_IAM
+obs.local.invoke:
+	sam local invoke --profile ${PROFILE} -t ${OBS_TEMPLATE} --parameter-overrides ${OBS_PARAMS} --env-vars etc/environment.json -e etc/local.json FnProducer | jq
+obs.invoke:
 	aws --profile ${PROFILE} lambda invoke --function-name ${OUT_FN} --invocation-type RequestResponse --payload file://etc/event.json --cli-binary-format raw-in-base64-out --log-type Tail tmp/fn.json | jq "." > tmp/response.json
 	cat tmp/response.json | jq -r ".LogResult" | base64 --decode
 
@@ -18,3 +18,9 @@ layer.xray:
 	pip install -r req_xray.txt --target=tmp/xray/python --upgrade
 layer.redis:
 	pip install -r req_redis.txt --target=tmp/redis/python --upgrade
+
+redis: redis.package redis.deploy
+redis.package:
+	sam package -t ${REDIS_TEMPLATE} --output-template-file ${REDIS_OUTPUT} --s3-bucket ${S3BUCKET}
+redis.deploy:
+	sam deploy -t ${REDIS_OUTPUT} --stack-name ${REDIS_STACK} --parameter-overrides ${REDIS_PARAMS} --capabilities CAPABILITY_NAMED_IAM
--- src/fn_producer.py
+++ src/fn_producer.py
@@ -1,12 +1,10 @@
 import boto3
 import json
 import os
+import redis
 from aws_xray_sdk.core import xray_recorder
 from aws_xray_sdk.core import patch_all
-
-# function: initialization
-def initialization():
-    patch_all()
+from datetime import datetime
 
 # function: helper
 def build_response(code, body):
@@ -24,7 +22,7 @@ def build_response(code, body):
     }
     return response
 
-# function: custom logic
+# function: external calls
 def send_message(payload):
     response = client.send_message(
         QueueUrl=queue_url,
@@ -32,23 +30,44 @@ def send_message(payload):
     )
     return response
 
+def rset(k, v):
+    return redis_connection.set(k, json.dumps(v))
+
+def rget(k):
+    output = redis_connection.get(k)
+    output = json.loads(output.decode("utf-8")) if output is not None else None
+    return output
+
+def rdelete(k):
+    return redis_connection.delete(k)
+
 # function: lambda invoker handler
 def handler(event, context):
     print(json.dumps(event))
 
     body = json.loads(event["body"])
+    event_id = body["id"]
     print(json.dumps(body))
     response = send_message(json.dumps(body))
     print(json.dumps(response))
     status = response["ResponseMetadata"]["HTTPStatusCode"]
     message_id = response["MessageId"]
 
-    subsegment = xray_recorder.begin_subsegment("CustomSubsegment")
+    now = datetime.today().isoformat()
+    subsegment = xray_recorder.begin_subsegment("Redis")
+    subsegment.put_annotation("EventId", event_id)
+    previous = rget(event_id)
+    rset(event_id, now) if redis_enabled else None
+    xray_recorder.end_subsegment()
+
+    subsegment = xray_recorder.begin_subsegment("GenerateResponse")
     subsegment.put_annotation("MessageId", message_id)
     payload = {
-        "event_id": body["id"],
+        "event_id": event_id,
         "event_body": body["message"],
         "sqs_message_id": message_id,
+        "ts_previous": previous,
+        "ts_current": now
     }
     response = build_response(status, json.dumps(payload))
     print(json.dumps(response))
@@ -56,6 +75,18 @@ def handler(event, context):
     return response
 
 # initialization, mapping
+patch_all()
 client = boto3.client("sqs")
 queue_url = os.environ["QUEUE_URL"]
-initialization()
+redis_enabled = os.environ["REDIS_ENABLED"] == "true"
+redis_endpoint = os.environ["REDIS_ENDPOINT"]
+try:
+    if redis_enabled:
+        redis_connection = redis.Redis(
+            host=redis_endpoint,
+            port=6379,
+            db=0,
+            socket_timeout=5
+        )
+except redis.ConnectionError as e:
+    print(e)
